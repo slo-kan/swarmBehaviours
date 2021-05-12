6a7cbb876ef563345d07a0646c42b400b5334dfb
NIlS-Z <44974491+NIlS-Z99@users.noreply.github.com>
2021-05-12 16:30:07 +0200

    Update attractionRepulsion.c

---
+++ attractionRepulsion.c
@@ -0,0 +1,17 @@
+// assume we have a position array of 50 positions (of other aircrafts) and the 
+// position of our own aircraft how do we implement the attraction repulsion 
+// swarm behaviour for a copter
+
+/* given this:
+
+ struct Pos {
+     double x;
+     double y;
+     double z;
+ }
+ Pos pos[] = new Pos[50];
+ Pos ownPos = new Pos;
+ 
+ implement the swarm behaviour
+*/
+
