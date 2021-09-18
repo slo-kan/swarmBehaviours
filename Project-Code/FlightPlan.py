9b11da3ed3c2b80228f9214bf7627bd4b7093f6e
Nils-Treuheit <75490938+Nils-Treuheit@users.noreply.github.com>
2021-09-18 05:12:03 +0200

    working tested version
    works on my machine

--- Project-Code/FlightPlan.py
+++ Project-Code/FlightPlan.py
@@ -16,7 +16,6 @@ from pprzlink.ivy import IvyMessagesInterface
 from pprzlink.message import PprzMessage
 
 
-
 #####################
 # Class Definitions #
 #####################
@@ -34,38 +33,6 @@ class Point:
     
     def __setitem__(self,idx,val:float):
         self.__coords[self.__IDXS[idx]] = val
-
-
-# Simple but thread safe logger
-class ThreadSafe_Logger:
-    __QUEUE:Queue = Queue()
-    __finished:bool = False
-
-    def __write_to_file(self):
-        while not self.__finished or not self.__QUEUE.empty():
-            try: data = self.__QUEUE.get(timeout=0.2)
-            except Empty: time.sleep(1)
-            else: 
-                self.__FILE_WRITER.write(data)
-                self.__QUEUE.task_done()
-
-    def __init__(self, filename):
-        self.__FILENAME = filename
-        self.__WORKER = Thread(target=self.__write_to_file,name="Logging_Thread")
-    
-    def start(self): 
-        self.__FILE_WRITER = open(self.__FILENAME,"a")
-        self.__WORKER.start()
-        self.write("Logging_Thread started...\n")
-
-    def write(self, data:str): self.__QUEUE.put(data)
-    def close(self):
-        self.write("... Logging_Thread closed!\n")
-        self.__finished = True
-        self.__QUEUE.join()
-        self.__WORKER.join()
-        self.__FILE_WRITER.close() 
-
 
 
 #Input Function
@@ -90,150 +57,151 @@ def get_szenario(filename)->"tuple[list[Point]]":
     return (att_points,rep_points)
 
 
-#global constants 
-LOGGER = ThreadSafe_Logger("/home/finkensim/finken/paparazzi/sw/tools/ovgu_swarm/WP_Mover.log")
-ATT_POINTS, REP_POINTS = get_szenario("flight_plan.cvs")
-START_ID, END_ID = (int(30),int(40))                #end id is exclusiv 40 means last copter in the swarm has id 39
-ATT_ID, REP_ID = (int(2),int(3))
-MSG_ACCESS:Lock = Lock()
+#global constants
+def CURRENT_TIME()->float: return time.time()
 GLOBE_RADIUS:int = int(6371000)
-INTERFACE = IvyMessagesInterface(
-                agent_name="Pprzlink_Move_WP",      # Ivy agent name
-                start_ivy=False,                    # Do not start the ivy bus now
-                ivy_bus="127.255.255.255:2010"      # address of the ivy bus
-            )
-
-#global variables
-moveWP:"list[PprzMessage]" = []
-terminate :bool   = False
-epoche    :int    = 0
-#counter  :int    = 1
+TIMER = 40
 
 
 
+class WP_Mover:
+    #class-global consts
+    __SENDING:Lock = Lock()
+    __ATT_ID, __REP_ID = (int(2),int(3))
+
+    #class-global variables
+    __moveWP:"list[PprzMessage]" = []
+    __epoche :int = 0
+    __counter:int = 1
 
 
-########################
-# Function Definitions #
-########################
 
-#converts a LLA-Point from float radial representation to a int degree representation
-def convertToInt(point:Point)->"tuple[int]":
-    lat = int((point["lat"]*180/math.pi)*1e7)
-    lon = int((point["lon"]*180/math.pi)*1e7)
-    alt = int(point["alt"])
-    return (lat,lon,alt)
+    ########################
+    # Function Definitions #
+    ########################
 
+    def __init__(self, start_id:int, end_id:int, filename:str, debug:bool=False):
+        self.__ATT_POINTS, self.__REP_POINTS = get_szenario(filename)
+        self.__START_ID = start_id  #start id is inclusiv -> 30 means first copter in the swarm has id 30
+        self.__END_ID = end_id  #end id is exclusiv -> 40 means last copter in the swarm has id 39
 
-#converts a LLA-Point from float radial representation to float degree representation
-def convertToDegree(point:Point)->"tuple[float]":
-    lat = float(point["lat"]*180/math.pi)
-    lon = float(point["lon"]*180/math.pi)
-    alt = float(point["alt"])
-    return (lat,lon,alt)
+        for acId in range(self.__START_ID, self.__END_ID):
+            self.__moveWP.append(WP_Mover.__createMSG(self.__ATT_ID,acId,self.__ATT_POINTS[0]))
+            self.__moveWP.append(WP_Mover.__createMSG(self.__REP_ID,acId,self.__REP_POINTS[0]))
 
+        with open("/home/finkensim/finken/paparazzi/sw/tools/ovgu_swarm/WP_Mover.log","w") as LOGGER:
+            LOGGER.write("Start FlightPlan...\n")
 
-#create PprzMessage            
-def createMSG(wp_id:int,ac_id:int,point:Point)->"PprzMessage":
-    msg = PprzMessage("ground", "MOVE_WAYPOINT")
-    msg['wp_id'] = wp_id
-    msg['ac_id'] = str(ac_id)
-    msg['lat'],msg['lon'],msg['alt'] = convertToDegree(point)
-    return msg
+        self.__DEBUG = debug
+        self.__INTERFACE = IvyMessagesInterface("Pprzlink_Move_WP")
 
 
-#send update msgs through interface
-def send_msgs():
-    global moveWP, terminate
+    def __del__(self):
+        self.__INTERFACE.unsubscribe(self.__bind_id)
+        with open("/home/finkensim/finken/paparazzi/sw/tools/ovgu_swarm/WP_Mover.log","a") as LOGGER: 
+            LOGGER.write("... STOPPED FlightPlan!\n")
+        self.__INTERFACE.shutdown()
+
+
+    #converts a LLA-Point from float radial representation to a int degree representation
+    @staticmethod
+    def convertToInt(point:Point)->"tuple[int]":
+        lat = int((point["lat"]*180/math.pi)*1e7)
+        lon = int((point["lon"]*180/math.pi)*1e7)
+        alt = int(point["alt"])
+        return (lat,lon,alt)
     
-    LOGGER.write("MSG-Thread started ... \n")
-    while not terminate:
-        aquired = MSG_ACCESS.acquire(timeout=2.0)
-        if aquired:
-            LOGGER.write("len(moveWP) = %d\n" % len(moveWP))
+
+    #converts a LLA-Point from float radial representation to float degree representation
+    @staticmethod
+    def convertToDegree(point:Point)->"tuple[float]":
+        lat = float(point["lat"]*180/math.pi)
+        lon = float(point["lon"]*180/math.pi)
+        alt = float(point["alt"])
+        return (lat,lon,alt)
+    
+
+    #gets the metric distance between two points given in the LlaCoor_f format
+    @staticmethod
+    def getDistance(own_pos:Point, goal_pos:Point)->float:
+        return float( GLOBE_RADIUS * math.acos(                  
+            math.sin(own_pos["lat"]) * math.sin(goal_pos["lat"]) +    
+            math.cos(own_pos["lat"]) * math.cos(goal_pos["lat"]) *    
+            math.cos(own_pos["lon"] - goal_pos["lon"])
+        ))
+
+
+    #create PprzMessage 
+    @staticmethod           
+    def __createMSG(wp_id:int,ac_id:int,point:Point)->"PprzMessage":
+        msg = PprzMessage("ground","MOVE_WAYPOINT")
+        lat,lon,alt = WP_Mover.convertToDegree(point)
+        msg.set_values([str(ac_id), wp_id, lat, lon, alt])
+        return msg
+
+
+    #function for sending msgs periodically
+    def __send_msgs(self):
+        if self.__SENDING.acquire(timeout=0.1):
             string = ""
-            for i,msg in enumerate(moveWP):
-                time.sleep(0.2)
-                INTERFACE.send(msg)
-                pre = str("ATT" if ATT_ID == msg['wp_id'] else "REP")
+            for i,msg in enumerate(self.__moveWP):
+                time.sleep(0.01)
+                self.__INTERFACE.send(msg)
+                pre = str("ATT" if msg['wp_id'] == self.__ATT_ID  else "REP")
                 string += ("%2d. %3s-WP_Move-MSG: %s\n" % (i, pre, msg))
-            LOGGER.write(string)
-            MSG_ACCESS.release()
-            time.sleep(3)
-    LOGGER.write("... MSG-Thread closed! \n")
-
-#create constant global MSG_SENDING_THREAD 
-MSG_SENDING_THREAD = Thread(target=send_msgs,name="Send_Msg_Thread")
+            if self.__DEBUG:
+                with open("/home/finkensim/finken/paparazzi/sw/tools/ovgu_swarm/WP_Mover.log","a") as LOGGER:
+                    LOGGER.write("len(moveWP) = %d\n" % len(self.__moveWP))
+                    LOGGER.write(string)
+            self.__SENDING.release()
 
 
-#gets the metric distance between two points given in the LlaCoor_f format
-def getDistance(own_pos:Point, goal_pos:Point)->float:
-    return float( GLOBE_RADIUS * math.acos(                  
-        math.sin(own_pos["lat"]) * math.sin(goal_pos["lat"]) +    
-        math.cos(own_pos["lat"]) * math.cos(goal_pos["lat"]) *    
-        math.cos(own_pos["lon"] - goal_pos["lon"])
-    ))
+    #main method - executes logic
+    def run(self):
 
-
-#manipulate goal points via msg updates
-def recv_callback(ac_id, recvMsg):
-    global epoche, moveWP#, counter
-
-    if(int(recvMsg["achieved"])>0):
-        own_pos = Point(float(recvMsg["lat"]),float(recvMsg["lon"]),float(recvMsg["alt"]))
-        #out = open("/home/finkensim/finken/paparazzi/sw/tools/ovgu_swarm/WP_Mover.log","a")
-        #out.write("%d. Goal_Achieved-MSG: %s\n" % (counter, recvMsg))
-        #out.close()
-        #counter+=1
-
-        if(getDistance(own_pos,ATT_POINTS[epoche%len(ATT_POINTS)])<=1.5):
-            aquired = MSG_ACCESS.acquire(timeout=3.0)
-            if aquired:
-                epoche += 1
-                moveWP = []
-                for acId in range(START_ID, END_ID):
-                    moveWP.append(createMSG(ATT_ID,acId,ATT_POINTS[epoche%len(ATT_POINTS)]))
-                    moveWP.append(createMSG(REP_ID,acId,REP_POINTS[epoche%len(REP_POINTS)]))
-                LOGGER.write("%d send this valid Goal_Achieved-MSG: %s\n" % (ac_id, recvMsg))
-                LOGGER.write("%d. Epoche - " % epoche)
-                MSG_ACCESS.release()
-            
-
-#main method
-def main():
-    global moveWP, terminate
-
-    out = open("/home/finkensim/finken/paparazzi/sw/tools/ovgu_swarm/WP_Mover.log","w")
-    out.write("Start FlightPlan...\n")
-    out.close()
-    time.sleep(3)
-
-    #init vars
-    LOGGER.start()
-    with MSG_ACCESS:
-        for acId in range(START_ID, END_ID):
-            moveWP.append(createMSG(ATT_ID,acId,ATT_POINTS[0]))
-            moveWP.append(createMSG(REP_ID,acId,REP_POINTS[0]))    
-    LOGGER.write(str("Start_ID: "+str(START_ID)+"; End_ID: "+str(END_ID)+"; moveWP length: "+str(len(moveWP))+"\n"))
-    LOGGER.write("Init-Epoche - ")
-
-    #run program
-    try:
-        INTERFACE.start()
-        INTERFACE.subscribe(recv_callback,PprzMessage("telemetry", "GOAL_ACHIEVED"))
-        MSG_SENDING_THREAD.start()
-        time.sleep(15)
-        raise KeyboardInterrupt
-    except KeyboardInterrupt: LOGGER.write("... !STOPPING FlightPlan! ...\n")
-    finally:
-        terminate = True
-        MSG_SENDING_THREAD.join()
-        time.sleep(3)
-        LOGGER.close()
-        INTERFACE.shutdown()
+        #manipulate goal points via msg updates
+        def recv_callback(ac_id, recvMsg):
+            change = False
+            if(int(recvMsg["achieved"])>0):
+                own_pos = Point(float(recvMsg["lat"]),float(recvMsg["lon"]),float(recvMsg["alt"]))
+                if(WP_Mover.getDistance(own_pos,self.__ATT_POINTS[self.__epoche%len(self.__ATT_POINTS)])<=1.5):
+                    change = True
+                    self.__epoche += 1
+                    with open("/home/finkensim/finken/paparazzi/sw/tools/ovgu_swarm/WP_Mover.log","a") as LOGGER:
+                        LOGGER.write("%d send this valid Goal_Achieved-MSG: %s\n" % (ac_id, recvMsg))
+                        LOGGER.write("%d. Epoche...\n" % self.__epoche)
+            if (ac_id == self.__START_ID) or change:
+                self.__moveWP = []
+                for acId in range(self.__START_ID, self.__END_ID):
+                    self.__moveWP.append(WP_Mover.__createMSG(self.__ATT_ID,acId,self.__ATT_POINTS[self.__epoche%len(self.__ATT_POINTS)]))
+                    self.__moveWP.append(WP_Mover.__createMSG(self.__REP_ID,acId,self.__REP_POINTS[self.__epoche%len(self.__REP_POINTS)]))
+                if self.__DEBUG: 
+                    with open("/home/finkensim/finken/paparazzi/sw/tools/ovgu_swarm/WP_Mover.log","a") as LOGGER:
+                        LOGGER.write("%d. Goal_Achieved-MSG: %s\n" % (self.__counter, recvMsg))
+                        self.__counter+=1
+                self.__send_msgs()
         
+
+        #log infos about init state
+        with open("/home/finkensim/finken/paparazzi/sw/tools/ovgu_swarm/WP_Mover.log","a") as LOGGER:   
+            LOGGER.write(str("Start_ID: "+str(self.__START_ID)+"; End_ID: "+str(self.__END_ID)+"; moveWP length: "+str(len(self.__moveWP))+"\n"))
+            if self.__DEBUG: 
+                for i,msg in enumerate(self.__moveWP): LOGGER.write("%2d.MSG: ac_id = %2d; type = %s\n" % (i,msg["ac_id"],str(type(msg["ac_id"]))))
+            LOGGER.write("Init-Epoche...\n")
+
+        #run program
+        self.__INTERFACE.start()
+        time.sleep(2.5)
+        self.__send_msgs()
+        self.__bind_id = self.__INTERFACE.subscribe(recv_callback,PprzMessage("telemetry", "GOAL_ACHIEVED"))
+
 
 
 #main program
 if __name__=='__main__':
-    main()
+    wp_mover:WP_Mover = WP_Mover(30, 40, "flight_plan.cvs")
+    wp_mover.run()
+    time.sleep(40)
+    del wp_mover
+    wp_mover = None
+    sys.exit()
